e82cc01534c29db7940754f0f1fc1f39d85b6a24
DANIEL <dandrichest@gmail.com>
2026-05-10 23:07:13 +0100

    Update connection

--- obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79e7d7d8600de7281bb9c0ca020aa4889c6fdd74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf0eddcc7285e72e2e89da6b9d9cdbc258cce625")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
